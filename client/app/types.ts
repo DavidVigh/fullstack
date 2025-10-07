b8df33d2c4562be77528aeb33d72446f71978210
Vigh David <david.vigh08@gmail.com>
2025-10-07 13:32:33 +0200

    updated types

--- client/app/types.ts
+++ client/app/types.ts
@@ -12,10 +12,16 @@ export type Post = {
     author: string;
     userId: number;
     Tags: Tag[];
+    date: string;
     content: string;
+    User: User;
 };
 
 export type Tag = {
     id: number;
     name: string;
+};
+
+export type TagsDestruct = {
+    tags: Tag[];
 };
